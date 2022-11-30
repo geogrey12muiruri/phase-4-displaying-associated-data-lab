8bd1f16fd893e1652781fead2cdc616dd72e6591
geogrey12muiruri <greggambugua@gmail.com>
2022-11-30 17:09:07 +0300

    commit tree 55a001a36c9b3e065d5f412355e086e89312a401

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+    #returns an array of all items with user info
+    #returns an array of all items with user info
+    def index
+        render json: Item.all, include: :user
+    end
 end
